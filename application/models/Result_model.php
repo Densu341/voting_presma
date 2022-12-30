a34caa3f34a31c0569b74ccd4f56740ef3603ff3
ryn-crypto <riyanfirst13@gmail.com>
2022-12-31 00:29:52 +0700

    memperbaiki tampilan hasil voting

--- application/models/Result_model.php
+++ application/models/Result_model.php
@@ -5,9 +5,9 @@ class Result_model extends CI_Model
 {
     public function get_result()
     {
-        $this->db->select('candidate.id_candidate, candidate.name, candidate.photo, COUNT(vote.id_candidate) AS total_vote');
+        $this->db->select('candidate.id_candidate, candidate.nama_candidate, candidate.foto, COUNT(result.id_candidate) AS total_vote');
         $this->db->from('candidate');
-        $this->db->join('vote', 'vote.id_candidate = candidate.id_candidate', 'left');
+        $this->db->join('result', 'result.id_candidate = candidate.id_candidate', 'left');
         $this->db->group_by('candidate.id_candidate');
         $this->db->order_by('total_vote', 'DESC');
         return $this->db->get()->result_array();
--- application/views/committee/result.php
+++ application/views/committee/result.php
@@ -4,29 +4,48 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-    <table class="table-responsive">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">NIM</th>
-                <th scope="col">ID Candidate</th>
-                <th scope="col">Take</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php $no = 1; ?>
-            <?php foreach ($result as $r) : ?>
-                <tr>
-                    <th scope="row"><?= $no; ?></th>
-                    <td><?= $r['nim']; ?></td>
-                    <td><?= $r['nama_candidate']; ?></td>
-                    <td><?= $r['take']; ?></td>
-                </tr>
-                <?php $no++; ?>
+    <div class="container-fluid">
+        <div class="row mb-3">
+            <?php foreach ($persentase as $p) : ?>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="<?= base_url() ?>assets/img/candidate/<?= $p['foto']; ?>" alt="Card image cap">
+                        <div class="card-body text-center">
+                            <h5 class="card-title"><?= $p['nama_candidate']; ?></h5>
+                            <p class="card-text">mendapatkan</p>
+                            <span class="btn btn-primary d-flex justify-content-center"><?= (int)(($p['total_vote']/$jml)*100) ?> % <br> dengan perolehan <?= $p['total_vote']; ?> suara</span>
+                        </div>
+                    </div>
+                </div>
             <?php endforeach; ?>
-        </tbody>
+        </div>
+    </div>
 
-    </table>
+    <div class="container-fluid mt-5">
+        <h3>Rincian data pemilihan</h3>
+        <table class="table table-responsive table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">NIM</th>
+                    <th scope="col">ID Candidate</th>
+                    <th scope="col">Take</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $no = 1; ?>
+                <?php foreach ($result as $r) : ?>
+                    <tr>
+                        <th scope="row"><?= $no; ?></th>
+                        <td><?= $r['nim']; ?></td>
+                        <td><?= $r['nama_candidate']; ?></td>
+                        <td><?= $r['take']; ?></td>
+                    </tr>
+                    <?php $no++; ?>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 
 </div>
 <!-- /.container-fluid -->
